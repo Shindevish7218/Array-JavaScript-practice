d4ab8ae2872c1af3d7d69d496f2ac5d9159a023d
Shindevish7218 <shindevish7218@gmail.com>
2023-09-13 17:14:01 +0530

    first commit

---
+++ array.js
@@ -0,0 +1,35 @@
+// var arr = [11,22,44,55,66,77,88,99]
+// console.log(arr)
+// console.log[0,3]
+
+
+// var arr=[10]
+// var arr1 = arr[0]
+// arr[0] = 20;
+// console.log(arr1)
+
+// var a ="7218vishnu";
+// console.log(parseInt(a))
+
+//var a= "72.181117" /String can not fix it it only for number/
+//console.log(a.toFixed(2));
+
+// var b= 83.6388383838;
+// console.log(b.toFixed(1));
+
+// var MName =("i Love \"Myself\"")
+// console.log(MName);
+
+ //What is concatanation ?
+//  var fName ="Vishnu    "
+//   var LName ="Shinde"
+//  console.log(fName.concat(LName))
+//  console.log(fName.trim(" "))
+//  console.log(fName.charAt(3));
+//  var days ="mon tues wed thu"
+//  console.log(days.split(" "))
+//   var mstr ='MO Tu We Th Fr Sa Su'
+//   console.log(mstr.toUpperCase().split(" "))
+var str =" i  love JavaScript"
+console.log(str.indexOf("i",3))
+console.log(str.lastIndexOf("S"))
